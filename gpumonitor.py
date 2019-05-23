416e60454bbdb9c7944e331aa69e270d5c036022
Yuto Ogino <fujjisaaan@gmail.com>
2019-05-23 18:39:10 +0900

    reformat the code

--- gpumonitor.py
+++ gpumonitor.py
@@ -11,7 +11,7 @@ import requests
 
 load_dotenv()
 mattermostIncomingWebhook = os.getenv("WEBHOOK")
-nvidiaLogoLink=""
+nvidiaLogoLink = ""
 
 # Saves all known and already published jobs, so that they get not published again
 saveKnownPIDS = []
@@ -23,86 +23,98 @@ saveKnownDic = dict()
 showStatusUpdate = False
 
 while (True):
-  showStatusUpdate = False
+    showStatusUpdate = False
 
-  # List contains jobs seen at this time, so that one can find and show finished jobs
-  seenPIDS = []
+    # List contains jobs seen at this time, so that one can find and show finished jobs
+    seenPIDS = []
 
+    pidfinding = subprocess.Popen(['nvidia-smi', '--query-compute-apps=pid,process_name,used_memory',
+                                   '--format=csv,noheader'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pidProcessMemorystring = pidfinding.stdout.read()
+    pidProcessMemorys = pidProcessMemorystring.split('\n')
 
-  pidfinding=subprocess.Popen(['nvidia-smi', '--query-compute-apps=pid,process_name,used_memory', '--format=csv,noheader'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  pidProcessMemorystring=pidfinding.stdout.read()
-  pidProcessMemorys=pidProcessMemorystring.split('\n')
+    for info in pidProcessMemorys:
+        infodetail = info.split(',')
+        if(len(infodetail) == 3):
+            pid = infodetail[0]
+            processName = infodetail[1]
+            processMemory = infodetail[2]
+            if pid.isdigit() and pid not in saveKnownPIDS:
+                # the /proc/PID is owned by process creator
+                proc_stat_file = os.stat("/proc/%d" % int(pid))
+                # get UID via stat call
+                uid = proc_stat_file.st_uid
+                # look up the username from uid
+                username = pwd.getpwuid(uid)[0]
 
-  
-  for info in pidProcessMemorys:
-    infodetail=info.split(',')
-    if(len(infodetail) == 3):
-      pid=infodetail[0]
-      processName=infodetail[1]
-      processMemory=infodetail[2]
-      if pid.isdigit() and pid not in saveKnownPIDS:
-        # the /proc/PID is owned by process creator
-        proc_stat_file = os.stat("/proc/%d" % int(pid))
-        # get UID via stat call
-        uid = proc_stat_file.st_uid
-        # look up the username from uid
-        username = pwd.getpwuid(uid)[0]
-        
-	# Create string
-        messageString = "### New GPU Job\n User **@" +  username + "** has created a job named **" + processName + "** on GPU with pid **" + pid + "** consuming **" + processMemory + "** memory."
-        print(messageString)
+                # Create string
+                messageString = "### New GPU Job\n User **@" + username + "** has created a job named **" + \
+                    processName + "** on GPU with pid **" + pid + \
+                    "** consuming **" + processMemory + "** memory."
+                print(messageString)
 
-        messageData={'text': messageString, 'icon_url': nvidiaLogoLink}
-	print(messageData)
-	response = requests.post(mattermostIncomingWebhook, data=json.dumps(messageData), headers={'Content-Type': 'application/json'})
+                messageData = {'text': messageString,
+                               'icon_url': nvidiaLogoLink}
+                print(messageData)
+                response = requests.post(mattermostIncomingWebhook, data=json.dumps(
+                    messageData), headers={'Content-Type': 'application/json'})
+                if response.status_code != 200:
+                    print('Request error ', response.status_code,
+                          ' the response is:\n', response.text)
+
+                # Activate statusUpdate
+                showStatusUpdate = True
+
+                saveKnownPIDS.append(pid)
+
+                # Save job info in dictionary to notify user later  about finished job
+                tempList = [username, processName]
+                if pid not in saveKnownDic:
+                    saveKnownDic[pid] = list()
+                    saveKnownDic[pid] = tempList
+                print(saveKnownDic)
+
+            if pid.isdigit():
+                seenPIDS.append(pid)
+
+    # Show status update after last new job is published
+    if showStatusUpdate:
+        statusFinding = subprocess.Popen(
+            ['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        currentStatus = statusFinding.stdout.read()
+        currentStatusString = '### Status after starting new jobs\n ``` \n' + \
+            currentStatus + '\n ```'
+        print(currentStatusString)
+        statusData = {'text': currentStatusString, 'icon_url': nvidiaLogoLink}
+        print(statusData)
+
+        response = requests.post(mattermostIncomingWebhook, data=json.dumps(
+            statusData), headers={'Content-Type': 'application/json'})
         if response.status_code != 200:
-          print('Request error ', response.status_code, ' the response is:\n', response.text)
+            print('Request error ', response.status_code,
+                  ' the response is:\n', response.text)
 
-	# Activate statusUpdate
-        showStatusUpdate = True
+    # Compare seenPIDS with knownPIDS and notify user if job is finished
+    finishedPIDS = list(set(saveKnownPIDS) - set(seenPIDS))
+    for pid in finishedPIDS:
+        username, processName = saveKnownDic.get(pid)
 
-        saveKnownPIDS.append(pid)
+        if username != None and processName != None:
+            # Create string
+            messageString = "### Finished GPU Job\n User **@" + username + \
+                "** your job named **" + processName + \
+                "** with pid **" + pid + "** has finished."
+            print(messageString)
 
-        # Save job info in dictionary to notify user later  about finished job
-	tempList=[username, processName]
-	if pid not in saveKnownDic:
-          saveKnownDic[pid] = list()
-          saveKnownDic[pid] = tempList
-        print(saveKnownDic)
+            messageData = {'text': messageString, 'icon_url': nvidiaLogoLink}
+            print(messageData)
+            response = requests.post(mattermostIncomingWebhook, data=json.dumps(
+                messageData), headers={'Content-Type': 'application/json'})
+            if response.status_code != 200:
+                print('Request error ', response.status_code,
+                      ' the response is:\n', response.text)
 
-      if pid.isdigit():
-        seenPIDS.append(pid)
+        saveKnownPIDS.remove(pid)
+        del saveKnownDic[pid]
 
-  # Show status update after last new job is published
-  if showStatusUpdate:
-    statusFinding=subprocess.Popen(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    currentStatus=statusFinding.stdout.read()
-    currentStatusString='### Status after starting new jobs\n ``` \n' + currentStatus + '\n ```'
-    print(currentStatusString)
-    statusData={'text': currentStatusString, 'icon_url': nvidiaLogoLink}
-    print(statusData)
-    
-    response = requests.post(mattermostIncomingWebhook, data=json.dumps(statusData), headers={'Content-Type': 'application/json'})
-    if response.status_code != 200:
-      print('Request error ', response.status_code, ' the response is:\n', response.text)
-
-  # Compare seenPIDS with knownPIDS and notify user if job is finished
-  finishedPIDS = list(set(saveKnownPIDS) - set(seenPIDS))
-  for pid in finishedPIDS:
-    username, processName = saveKnownDic.get(pid)
-
-    if username != None and processName != None: 
-      # Create string
-      messageString = "### Finished GPU Job\n User **@" +  username + "** your job named **" + processName + "** with pid **" + pid + "** has finished."
-      print(messageString)
-
-      messageData={'text': messageString, 'icon_url': nvidiaLogoLink}
-      print(messageData)
-      response = requests.post(mattermostIncomingWebhook, data=json.dumps(messageData), headers={'Content-Type': 'application/json'})
-      if response.status_code != 200:
-        print('Request error ', response.status_code, ' the response is:\n', response.text)
-
-    saveKnownPIDS.remove(pid)
-    del saveKnownDic[pid]
-
-  time.sleep(3)
+    time.sleep(3)
